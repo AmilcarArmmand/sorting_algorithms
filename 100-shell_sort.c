5357785aa07fc8e826801055d7af0b137468b63f
AmilcarArmmand <armmand@gmail.com>
2021-02-04 03:42:03 +0000

    draft of task 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Shell sort algorithm using Knuth sequence
+ * @array: Pointer to array being sorted.
+ * @size: Size of the array.
+ *
+ * Return: No value returned.
+ */
+void shell_sort(int *array, size_t size)
+{
+	int gap, i, j, temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < ((int)size / 3);)
+		gap = gap * 3 + 1;
+
+	for (i = gap; i < (int)size; i++)
+	{
+		temp = array[i];
+		for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+		{
+			array[j] = array[j - gap];
+		}
+		array[j] = temp;
+		print_array(array, size);
+	}
+}
