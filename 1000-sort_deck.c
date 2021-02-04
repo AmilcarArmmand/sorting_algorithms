d70104f9c0e6fb35ff55dd04858c41686fe0f4cd
AmilcarArmmand <armmand@gmail.com>
2021-02-04 05:07:51 +0000

    added base file for 1000-sort_deck.c

---
+++ 1000-sort_deck.c
@@ -0,0 +1,13 @@
+#include "deck.h"
+
+/**
+ * sort_deck - Function that sorts a deck of cards
+ * @deck: pointer to a deck of cards
+ *
+ * Return: No value returned.
+ */
+void sort_deck(deck_node_t **deck)
+{
+
+	print_deck(*deck);
+}
